0398e62b797fd54588a058ea448ee83eb24d2606
Patrick_Williamson <patrickw9080@outlook.com>
2022-04-01 11:25:52 -0400

    Added registration route.

--- configs/dbconfig.js
+++ configs/dbconfig.js
@@ -1,10 +1,19 @@
 var sqlite3 = require('sqlite3').verbose();
+// Database file name var. Helps prevent typos.
 var dbFileName = './configs/marketdb.db';
 
+/**
+ * @description Opens and returns a sqlite3 databse object.
+ * @returns {Database} sqlite3 database.
+ */
 function openDatabase() {
     return new sqlite3.Database(dbFileName);
 };
 
+/**
+ * @description Initializes the database tables if they are NOT EXISTS.
+ * Since this uses sqlite3 this should be called when the api starts up.
+ */
 function initializeDatabase() {
     const TAG = 'dbconfig initializeDatabase(), ';
     console.log(TAG + "Doing the work.");
@@ -22,7 +31,7 @@ function initializeDatabase() {
             };
         });
         // Create users table.
-        db.run('CREATE TABLE IF NOT EXISTS users (email TEXT NOT NULL UNIQUE, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, date_created INTEGER NOT NULL)', function (err) {
+        db.run('CREATE TABLE IF NOT EXISTS users (email TEXT NOT NULL UNIQUE, username TEXT NOT NULL UNIQUE, hashed_password BLOB NOT NULL, salt BLOB, user_type TEXT NOT NULL, date_created TEXT NOT NULL)', function (err) {
             console.log('Table users: ', this);
             if (err) {
                 console.log('Error creating table users: ');
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,106 @@
+// Imports.
 var express = require('express');
 var router = express.Router();
+var dbConfig = require('../configs/dbconfig');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var crypto = require('crypto');
+
+// Passport configuration.
+passport.use(new LocalStrategy(function verify(email, password, cb) {
+  let db = dbConfig.openDatabase();
+  db.get('SELECT rowid as id, email, hashed_password, salt FROM users WHERE email = ?', [email], function (err, row) {
+    if (err) { return cb(err); }
+    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+      if (err) { return cb(err); }
+      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.' });
+      }
+
+      return cb(null, row);
+
+    });
+  });
+}));
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.email });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+
+
+
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-/**
- * POST register a new user account.
- */
+
+router.post('/', function (req, res, next) {
+  const TAG = '\nusers POST /(), ';
+  console.log(TAG + 'Is registering a new user.');
+
+  // Current timestamp date to sqlite friendly date.
+  let currentDate = new Date();
+  let sqliteDate = currentDate.toISOString();
+
+  console.log(req.body);
+
+  // Salt and hash.
+  let salt = crypto.randomBytes(16);
+  crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+    if (err) { return next(err); }
+
+    // Run db statement to insert user record.
+    let db = dbConfig.openDatabase();
+    console.log('Running db statement.')
+    db.run('INSERT INTO users (email, username, hashed_password, salt, user_type, date_created) VALUES (?, ?, ?, ?, ?, ?)', [
+      req.body.email,
+      req.body.username,
+      hashedPassword,
+      salt,
+      'Member',
+      sqliteDate
+    ], function (err) {
+      if (err) {
+        console.log('Error when running db statement: ');
+        console.log(err);
+        return next(err);
+      }
+
+      console.log('Db entry created.')
+      console.log(this);
+      var user = {
+
+        id: this.lastID,
+        username: req.body.email
+      };
+      // All good, send 200.
+      res.sendStatus(200);
+    })
+
+
+  });
+
+
+  //res.send('Dummy res, we got the data. Pong!');
+});
+
+
+
+
+
 
 module.exports = router;
